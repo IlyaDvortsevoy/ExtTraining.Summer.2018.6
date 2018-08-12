8db06df373a5b3a9f76c2cd57b55aa98b9139aa5
Ilya Dvortsevoy <links309@gmail.com>
2018-08-12 20:23:55 +0300

    + Person test class created

---
+++ GenericCollections.Tests/Person.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericCollections.Tests
+{
+    public class Person : IEquatable<Person>
+    {
+        public string FirstName { get; set; }
+
+        public string SecondName { get; set; }
+
+        public int Age { get; set; }
+
+        public bool Equals(Person other)
+            => other != null &&
+               this.FirstName.Equals(other.FirstName) &&
+               this.SecondName.Equals(other.SecondName) &&
+               this.Age.Equals(other.Age);
+
+        public override bool Equals(object other)
+        {
+            return this.Equals(other as Person);
+        }
+
+        public override int GetHashCode()
+        {
+            int hash = FirstName.GetHashCode()
+                + SecondName.GetHashCode()
+                + Age.GetHashCode();
+
+            return hash;
+        }
+    }
+}
